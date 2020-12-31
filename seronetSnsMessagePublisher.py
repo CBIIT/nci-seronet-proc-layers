b07f77a5af828adbfbb22e125c82dee16ae291d7
Fuyuan Wang <60631776+wfy1997@users.noreply.github.com>
2020-12-31 12:21:27 -0500

    Add files via upload

---
+++ seronetSnsMessagePublisher.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 31 11:45:40 2020
+
+@author: w1997
+"""
+
+import boto3
+import json
+
+
+
+def sns_publisher(result,TopicArn_Success,TopicArn_Failure):
+ try:
+  sns = boto3.client('sns')
+  # Publish a simple message to the specified SNS topic
+  #convert the result to json
+  resultJson=json.dumps(result)
+  #if copy successfully 
+  if(result['file_status']=="'COPY_SUCCESSFUL'"):
+    response = sns.publish(TopicArn=TopicArn_Success, Message=str(resultJson), MessageAttributes={"TargetCBC": {"DataType": "String", "StringValue": result['file_submitted_by']}} )
+  #if copy unsuccessfully     
+  elif(result['file_status']=="'COPY_UNSUCCESSFUL'"):
+    response = sns.publish(TopicArn=TopicArn_Failure, Message=str(resultJson), MessageAttributes={"TargetCBC": {"DataType": "String", "StringValue": result['file_submitted_by']}})
+  # return out the response
+  return response
+ except Exception as err:
+  raise err
