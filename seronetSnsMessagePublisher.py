bc8f13fa6bcbcf8f833c8db6bf99246e5b8f7de1
Fuyuan Wang <60631776+wfy1997@users.noreply.github.com>
2021-02-01 11:51:05 -0500

    Update seronetSnsMessagePublisher.py

--- seronetSnsMessagePublisher.py
+++ seronetSnsMessagePublisher.py
@@ -14,12 +14,13 @@ def sns_publisher(result,TopicArn_Success,TopicArn_Failure):
  try:
   sns = boto3.client('sns')
   # Publish a simple message to the specified SNS topic
-  #if copy successfully 
+  #if copy successfully
+  resultJson=json.dumps(result)
   if(result['file_status']=="'COPY_SUCCESSFUL'" or result['file_status']=='FILE_Processed'):
-    response = sns.publish(TopicArn=TopicArn_Success, Message=str(result), MessageAttributes={"TargetCBC": {"DataType": "String", "StringValue": result['file_submitted_by']}} )
+    response = sns.publish(TopicArn=TopicArn_Success, Message=str(resultJson), MessageAttributes={"TargetCBC": {"DataType": "String", "StringValue": result['file_submitted_by']}} )
   #if copy unsuccessfully     
   elif(result['file_status']=="'COPY_UNSUCCESSFUL'"):
-    response = sns.publish(TopicArn=TopicArn_Failure, Message=str(result), MessageAttributes={"TargetCBC": {"DataType": "String", "StringValue": result['file_submitted_by']}})
+    response = sns.publish(TopicArn=TopicArn_Failure, Message=str(resultJson), MessageAttributes={"TargetCBC": {"DataType": "String", "StringValue": result['file_submitted_by']}})
   # return out the response
   return response
  except Exception as err:
