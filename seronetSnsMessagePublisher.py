46048b4da67731fd1b768403acfeae150ad5576f
Fuyuan Wang <60631776+wfy1997@users.noreply.github.com>
2021-02-02 12:34:32 -0500

    Update seronetSnsMessagePublisher.py

--- seronetSnsMessagePublisher.py
+++ seronetSnsMessagePublisher.py
@@ -16,10 +16,10 @@ def sns_publisher(result,TopicArn_Success,TopicArn_Failure):
   # Publish a simple message to the specified SNS topic
   #if copy successfully
   resultJson=json.dumps(result)
-  if(result['file_status']=="'COPY_SUCCESSFUL'" or result['file_status']=='FILE_Processed'):
+  if(result['file_status']=="COPY_SUCCESSFUL" or result['file_status']=='FILE_Processed'):
     response = sns.publish(TopicArn=TopicArn_Success, Message=str(resultJson), MessageAttributes={"TargetCBC": {"DataType": "String", "StringValue": result['file_submitted_by']}} )
   #if copy unsuccessfully     
-  elif(result['file_status']=="'COPY_UNSUCCESSFUL'"):
+  elif(result['file_status']=="COPY_UNSUCCESSFUL"):
     response = sns.publish(TopicArn=TopicArn_Failure, Message=str(resultJson), MessageAttributes={"TargetCBC": {"DataType": "String", "StringValue": result['file_submitted_by']}})
   # return out the response
   return response
